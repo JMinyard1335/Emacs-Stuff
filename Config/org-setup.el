298fe015863d430118a560e6edf274a8c756174e
Jachin <jachinminyard@gmail.com>
2024-07-10 16:14:02 -0500

    Makeing Sure its upto date.

---
+++ Config/org-setup.el
@@ -0,0 +1,34 @@
+;;; org-setup.el --- Contains Configuration for org-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  
+
+;; Author:  <jachi@DEVTABLET>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This holds configurations for org-mode and org-agenda. Basically if its org it can be found in this file.
+;; This includes things like:
+;; 1. Org-mode
+;; 2. Org-Agenda
+;; 3. Org-Babel
+
+;;; Code:
+
+
+
+(provide 'org-setup)
+;;; org-setup.el ends here
