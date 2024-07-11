fba1766ca297ea5262120eff4ed522a62b5c0238
Jachin <jachinminyard@gmail.com>
2024-07-11 08:26:51 -0500

    Changed some Values and added the org-setup file.

--- Config/org-setup.el
+++ Config/org-setup.el
@@ -25,10 +25,79 @@
 ;; 1. Org-mode
 ;; 2. Org-Agenda
 ;; 3. Org-Babel
+;; 4. Org-Super-Agenda
+;; 5. Org-Modern
 
 ;;; Code:
+(defun my/org-mode-setup ()
+  (setq org-adapt-indentation t)
+  (visual-line-mode 1))
 
+(defun my/org-mode-visual-fill ()
+  (setq visual-fill-column-width 150
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
 
+(defun my/org-font-setup ()
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+
+(use-package org-capture)
+
+(use-package org
+  :init (setq inhibit-compacting-font-caches t)
+  :hook (org-mode . my/org-mode-setup)
+  :config
+  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+  (setq org-agenda-window-setup 'current-window)
+  (setq org-log-into-drawer t)
+  (setq org-log-done '('time))
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "IN-PROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")
+	  (sequence "ASSIGNED(A)" "CURRENT(C)" "|" "TURNEDIN(T)")))
+  (setq org-todo-keywords-for-agenda '("TODO(t)" "IN-PROGRESS(i)" "WAITING(w)" "ASSIGNED(A)" "CURRENT(C)"))
+  (setq org-done-keywords-for-agenda '("DONE(d)" "CANCELED(c)" "TURNEDIN(T)"))
+  (setq org-deadline-warning-days 7)
+  (setq org-agenda-hide-tags-regexp ".*")
+  (setq org-adapt-indentation t)
+  (setq org-agenda-tags-column 40)
+  (setq org-hide-drawer-startup t)
+  (my/org-font-setup))
+
+(use-package org-super-agenda
+  :ensure t
+  :hook (org-agenda-mode . org-super-agenda-mode))
+
+(use-package org-modern
+  :ensure t)
+
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package olivetti
+  :ensure t
+  :hook
+  (org-agenda-mode . olivetti-mode))
+
+(use-package doct
+  :ensure t
+  :commands (doct))
+
+(use-package visual-fill-column
+  :ensure t
+  :hook (org-mode . my/org-mode-visual-fill))
 
 (provide 'org-setup)
 ;;; org-setup.el ends here
--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 			 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-(add-to-list 'load-path "~/Emacs-Stuff/Config/")
+(add-to-list 'load-path "~/Documents/Emacs-Stuff/Config/")
 (use-package gui-setup
   :config
   (setq my-battery t)
@@ -17,23 +17,5 @@
   :config
   (which-key-mode))
 (use-package ligatures-setup)
-(use-package copilot-setup
-  :config
-  ;;(setq copilot--base-dir "~/.emacs.d/straight/repos/copilot.el/")
-  (setq copilot--server-executable ".emacs.d/.cache/copilot/lib/node_modules/copilot-node-server/bin/copilot-node-server"))
+(use-package copilot-setup)
 
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ligature rainbow-delimiters company-box company which-key highlight-indent-guides yasnippet doom-modeline)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
