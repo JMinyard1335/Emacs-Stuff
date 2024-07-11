e5d6f2ebcecb78c96d98eecfca9d5981a5a1f460
Jachin <jachinminyard@gmail.com>
2024-07-11 14:06:41 -0500

    fixed a copilot issue on linux.

--- Linux/Config/copilot-setup.el
+++ Linux/Config/copilot-setup.el
@@ -55,10 +55,7 @@
 	("C-<return>" . copilot-accept-completion)
 	("C-<tab>" . copilot-accept-completion-by-word))
   :config
-  (setq copilot-indent-offset-warning-disable t)
-  ;;(setq copilot--base-dir "~/.emacs.d/straight/repos/copilot.el/")
-  (setq copilot--server-executable "c:/DevCornor/home/jachi/.emacs.d/.cache/copilot/lib/node_modules/copilot-node-server/bin/copilot-node-server")
-  )
+  (setq copilot-indent-offset-warning-disable t))
 ;(setq copilot--server-executable "~/.emacs.d/.cache/copilot/lib/node_modules/copilot-node-server")
 (defun darken-color (color percent)
   "Darken a COLOR by a certain PERCENT."
